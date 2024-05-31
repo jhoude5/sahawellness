f192f366ceb1ff426a6b7a642d48d8f09781f5e1
Jennifer Aube <62511019+jaube05@users.noreply.github.com>
2024-05-31 11:10:52 -0400

    updating and styling desktop contact us page. adding font weight to inter google font

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,3 +1,5 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.min.js";
 import "@popperjs/core/dist/umd/popper.min.js";
+import "@fontsource/inter/500.css";
+import "@fontsource/inter/700.css";
--- src/pages/contact-us.js
+++ src/pages/contact-us.js
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import Layout from '../layouts/general';
 import '../sass/styles.scss';
+import BookNow from '../components/cta/booknow';
 
 const Contact = () => {
   const [name, setName] = useState("");
@@ -26,55 +27,44 @@ const Contact = () => {
         })
         .catch((error) => document.querySelector('.error').innerText = 'Something went wrong, pls try again.');
 };
-  // const data = useStaticQuery(graphql`
-  //   {
-  //     allContentfulBios {
-  //       nodes {
-  //         name
-  //         image {
-  //           url
-  //         }
-  //         description {
-  //           raw
-  //         }
-  //       }
-  //     }
-  //   }
-  // `);
-  // const cases = data.allContentfulBios.nodes;
-
   return (
     <Layout>
       
-      <section>
-      <form name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field"
+      <section className='contact-us-section'>
+        <div className='container'>
+            <div className='row'>
+            <h1>Contact Us</h1>
+      <form className='contact-us-form' name="contact" method="post" data-netlify="true" data-netlify-honeypot="bot-field"
                                 onSubmit={handleSubmit} id="contact-form">
                                 <input type="hidden" name="form-name" value="contact" />
                                 <input type="hidden" name="bot-field" />
                                 <div className="row clearfix">
-                                    <div className="col-md-6 col-sm-6 col-xs-12 form-group">
-                                        <input onChange={({ target }) => setName(target.value)}
+                                    <div className=" form-group">
+                                        <label className='col-lg-1'>Name <span className='required'>*</span></label>
+                                        <input  className='col-lg-11' onChange={({ target }) => setName(target.value)}
                                             type="text"
                                             required
                                             name="name"
                                             value={name}
-                                            minLength="3" placeholder="Your Name *" />
+                                            minLength="3" placeholder="Your Name" />
                                     </div>
 
-                                    <div className="col-md-6 col-sm-6 col-xs-12 form-group">
-                                        <input type="email"
+                                    <div className="form-group">
+                                        <label className='col-lg-1'>Email <span className='required'>*</span></label>
+                                        <input className='col-lg-11' type="email"
                                             name="email"
                                             value={email}
                                             required
-                                            onChange={({ target }) => setEmail(target.value)} placeholder="Your Email *" />
+                                            onChange={({ target }) => setEmail(target.value)} placeholder="Your Email" />
                                     </div>
 
-                                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group">
-                                        <textarea name="message" placeholder="Message *" required onChange={({ target }) => setMessage(target.value)}
+                                    <div className="form-group">
+                                        <label className='col-lg-1'>Message <span className='required'>*</span></label>
+                                        <textarea  className='col-lg-11' name="message" placeholder="Message" required onChange={({ target }) => setMessage(target.value)}
                                             value={message}></textarea>
                                     </div>
 
-                                    <div className="col-lg-12 col-md-12 col-sm-12 col-xs-12 form-group">
+                                    <div className="form-group form-button">
                                         <button className="theme-btn btn-style-one" type="submit">
                                             SUBMIT
                                         </button>
@@ -83,6 +73,9 @@ const Contact = () => {
                             </form>
                             <div className="success"></div>
                             <div className="error"></div>
+            </div>
+        </div>
+        <BookNow />
       </section>
 
 </Layout>
