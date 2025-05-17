4d77923329bd703b73bdcd3a96e223673b51927a
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-05-17 10:01:58 -0400

    Update main-menu.js

--- src/components/menus/mainmenu/main-menu.js
+++ src/components/menus/mainmenu/main-menu.js
@@ -14,7 +14,8 @@ const MainMenu = () => {
                 <li className='nav-item'><Link className='nav-link' to='/tour'>Tour</Link></li>
                 <li className='nav-item'><Link className='nav-link' to='/store'>Store</Link></li>
                 <li className='nav-item'><Link className='nav-link' to='/contact-us'>Contact us</Link></li>
-                <a className='book-appt-button btn btn-primary' href='https://jaddabboudacupuncture.janeapp.com/#staff_member/1'>Book an appointment</a>
+    <a className='book-appt-button btn btn-primary' href='https://continuumfitness.janeapp.com/#/staff_member/14'>Book Westboro</a>
+                <a className='book-appt-button btn btn-primary' href='https://jaddabboudacupuncture.janeapp.com/#staff_member/1'>Book Downtown</a>
             </ul>
         </div>
      
