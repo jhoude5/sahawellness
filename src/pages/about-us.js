5fb31a3fe30130c18a152896e51f832882edd43b
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-05-17 11:02:27 -0400

    Update about-us.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -31,11 +31,11 @@ const AboutUs = () => {
                     <div className='about-us-container'>
                       
                       <img className='owner-img' src={JaddPhoto} alt='Jadd Abboud'/>
-                      <p>A few years ago, Jadd suffered from insomnia. He averaged 2-3 hours of sleep a night. Although Jadd believes that Allopathic medicine has its place, he isn’t a fan of pharmaceuticals for insomnia.
-                      After trying a number of alternative treatments, Jadd found acupuncture and after the first session, slept 5 hours. After 3 sessions, Jadd was sleeping 7 hours consistently. Jadd immediately began to study alternative medicines, quickly becoming a Natural Health Practitioner at the Alternative Medicine College of Canada.
-                      He continued down this path and received a Doctorate in Oriental Medicine over the next 5 years. This path brought Jadd to Vancouver where he received his license to practice Acupuncture. This, in combination with his degree in Psychology, is the culmination of over a decade of study.
-                      Practicing since 2018, Jadd has helped people with sleeping and digestive issues, migraines/headaches, menstrual difficulties, anxiety, depression, and all sorts of body pain.
-                      Throughout this journey, Jadd trained in Cranial Sacral Therapy — a non-invasive, soothing therapy — that he typically incorporates in his treatments to bring balance to the Nervous System.
+                      <p>A few years ago, I suffered from insomnia. I averaged 2-3 hours of sleep a night. Although I believe that Allopathic medicine has its place, I'm not a fan of pharmaceuticals for insomnia.
+                      After trying a number of alternative treatments, I found Acupuncture and after the first session, slept 5 hours. After 3 sessions, I was sleeping 7 hours consistently. I immediately began to study alternative medicines, quickly becoming a Natural Health Practitioner at the Alternative Medicine College of Canada.
+                      I continued down this path and received a Doctorate in Oriental Medicine over the next 5 years. This path brought me to Vancouver where he received my license to practice Acupuncture. This, in combination with my degree in Psychology, is the culmination of over a decade of study.
+                      Practicing since 2018, I has help people with sleeping and digestive issues, migraines/headaches, menstrual difficulties, anxiety, depression, and all sorts of body pain.
+                      Throughout this journey, I trained in Cranial Sacral Therapy — a non-invasive, soothing therapy — that he typically incorporates in his treatments to bring balance to the Nervous System.
                       </p>
                     </div>
                 </div>
