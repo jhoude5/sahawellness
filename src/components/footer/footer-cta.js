edbe6c3263ab765b2be51df1f2dcecf6009e29b6
Jennifer Houde <jhoude5@yahoo.com>
2025-05-17 10:25:59 -0400

    Update footer-cta.js

--- src/components/footer/footer-cta.js
+++ src/components/footer/footer-cta.js
@@ -8,10 +8,10 @@ const FooterCTA = ({className}) =>  {
             <div className='row'>
                 <div className='row-item col-md-4'>
                 <h2 className='footer-cta-heading'>Location</h2>
-                <p>Westboro Location</p>
-     <p>310 Beechgrove Ave, Suite A, Ottawa</p>
-                    <p>Downtown Location</p>
-                        <p>190 Somerset Street West #208</p>
+                <p className="location-heading">Westboro Location</p>
+                <p>310 Beechgrove Ave, Suite A, Ottawa</p>
+                <p className="location-heading">Downtown Location</p>
+                <p>190 Somerset Street West #208</p>
             </div>
             <div className='row-item col-md-4'>
                 <h2 className='footer-cta-heading'>Contact</h2>
