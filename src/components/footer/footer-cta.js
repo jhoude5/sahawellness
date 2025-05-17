d52369c19643e2181e713b192dad2f309cecd619
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-05-17 10:36:43 -0400

    Update footer-cta.js

--- src/components/footer/footer-cta.js
+++ src/components/footer/footer-cta.js
@@ -9,7 +9,7 @@ const FooterCTA = ({className}) =>  {
                 <div className='row-item col-md-4'>
                 <h2 className='footer-cta-heading'>Location</h2>
                 <p className="location-heading">Westboro Location</p>
-                <p>310 Beechgrove Ave, Suite A, Ottawa</p>
+                <p>310 Beechgrove Ave, Suite A</p>
                 <p className="location-heading">Downtown Location</p>
                 <p>190 Somerset Street West #208</p>
             </div>
