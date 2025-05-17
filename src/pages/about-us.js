a260879eaf4ee7a2c53b3c2d2cf5e8be116648c3
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-05-17 11:12:24 -0400

    Update about-us.js

--- src/pages/about-us.js
+++ src/pages/about-us.js
@@ -33,9 +33,9 @@ const AboutUs = () => {
                       <img className='owner-img' src={JaddPhoto} alt='Jadd Abboud'/>
                       <p>A few years ago, I suffered from insomnia. I averaged 2-3 hours of sleep a night. Although I believe that Allopathic medicine has its place, I'm not a fan of pharmaceuticals for insomnia.
                       After trying a number of alternative treatments, I found Acupuncture and after the first session, slept 5 hours. After 3 sessions, I was sleeping 7 hours consistently. I immediately began to study alternative medicines, quickly becoming a Natural Health Practitioner at the Alternative Medicine College of Canada.
-                      I continued down this path and received a Doctorate in Oriental Medicine over the next 5 years. This path brought me to Vancouver where he received my license to practice Acupuncture. This, in combination with my degree in Psychology, is the culmination of over a decade of study.
-                      Practicing since 2018, I has help people with sleeping and digestive issues, migraines/headaches, menstrual difficulties, anxiety, depression, and all sorts of body pain.
-                      Throughout this journey, I trained in Cranial Sacral Therapy — a non-invasive, soothing therapy — that he typically incorporates in his treatments to bring balance to the Nervous System.
+                      I continued down this path and received a Doctorate in Oriental Medicine over the next 5 years. This path brought me to Vancouver where I received my license to practice Acupuncture. This, in combination with my degree in Psychology, is the culmination of over a decade of study.
+                      Practicing since 2018, I have help people with sleeping and digestive issues, migraines/headaches, menstrual difficulties, anxiety, depression, and all sorts of body pain.
+                      Throughout this journey, I trained in Cranial Sacral Therapy — a non-invasive, soothing therapy — that I typically incorporate in treatments to bring balance to the Nervous System.
                       </p>
                     </div>
                 </div>
