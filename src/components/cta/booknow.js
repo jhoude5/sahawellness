a0415ff103fb5c3df81c1a81f69b15796f8474c1
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-05-27 11:44:54 -0400

    Update booknow.js

--- src/components/cta/booknow.js
+++ src/components/cta/booknow.js
@@ -9,7 +9,7 @@ const BookNow = () =>  {
             <h2>Book now!</h2>
             <p>Discover integrative health in Kelowna. Let's start your journey.</p>
      <a href='https://continuumfitness.janeapp.com/#/staff_member/14' className='btn btn-primary'>Book Westboro</a>
-            <a href='https://jaddabboudacupuncture.janeapp.com/#staff_member/1' className='btn btn-primary'>Book Downtown</a>
+            <a href='https://jaddabboudacupuncture.janeapp.com/#staff_member/1' className='btn btn-primary'>Book Somerset</a>
         </div>
       </div>
    </section>
