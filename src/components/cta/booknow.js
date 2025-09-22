955edd4925d67d2c50ba0306fd04d22b1a83bd72
SahaWellnessAcupuncture <jaddabboudacu@gmail.com>
2025-09-22 10:49:26 -0400

    Update booknow.js

--- src/components/cta/booknow.js
+++ src/components/cta/booknow.js
@@ -7,7 +7,7 @@ const BookNow = () =>  {
       <div className='container booknow-section'>
         <div className='row'>
             <h2>Book now!</h2>
-            <p>Discover integrative health in Kelowna. Let's start your journey.</p>
+            <p>Discover integrative health in Ottawa. Let's start your journey.</p>
      <a href='https://continuumfitness.janeapp.com/#/staff_member/14' className='btn btn-primary'>Book Continuum Fitness</a>
             <a href='https://vervemovement.janeapp.com/#/staff_member/6' className='btn btn-primary'>Book Verve Movement</a>
         </div>
